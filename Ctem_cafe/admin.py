c97ae9ceb1bfa3520e6cebe55fbb5436f996f392
FelipeNunes <128396955+FelipeNMorgado@users.noreply.github.com>
2024-06-12 04:44:15 -0300

    Update admin.py

--- Ctem_cafe/admin.py
+++ Ctem_cafe/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Favorite3
 from .models import Cliente
 from .models import Cadastro2
+from .models import Noticias
 
 # Register your models here.
 admin.site.register(Favorite3)
 admin.site.register(Cliente)
 admin.site.register(Cadastro2)
+admin.site.register(Noticias)
