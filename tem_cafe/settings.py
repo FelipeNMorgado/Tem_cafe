ad859abcc011e4a905896f642a86ca8f065da5ea
Anderson Gomes <agrmv@cesar.school>
2024-06-11 23:57:06 -0300

    Update settings.py

--- tem_cafe/settings.py
+++ tem_cafe/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
 # Determine the target environment
-TARGET_ENV = os.getenv('TARGET_ENV')
-NOT_PROD = not TARGET_ENV.lower().startswith('prod')
+TARGET_ENV = os.getenv('TARGET_ENV', 'development').lower()
+NOT_PROD = TARGET_ENV != 'production'
 
 if NOT_PROD:
     # Development settings
@@ -27,8 +27,8 @@ else:
     # Production settings
     SECRET_KEY = os.getenv('SECRET_KEY')
     DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
-    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
-    CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split()
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
+    CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split()
     SECURE_SSL_REDIRECT = os.getenv('SECURE_SSL_REDIRECT', '0').lower() in ['true', 't', '1']
 
     if SECURE_SSL_REDIRECT:
